bd6c228bb25ccc6863a52a329f839d43ac269f78
Adam Neale <adam@adam-neale.co.uk>
2020-03-29 18:10:56 +0100

    fix: integer casting (#1)

--- spec/ByteParserSpec.php
+++ spec/ByteParserSpec.php
@@ -14,20 +14,24 @@ class ByteParserSpec extends ObjectBehavior
     public function it_parses_a_kilobyte_amount(): void
     {
         $this->parseBytes('1.00KB')->shouldReturn(1024);
+        $this->parseBytes('1.5KB')->shouldReturn(1536);
     }
 
     public function it_parses_a_megabyte_amount(): void
     {
-        $this->parseBytes('1.00MB')->shouldReturn(1024 * 1024);
+        $this->parseBytes('1.00MB')->shouldReturn(1048576);
+        $this->parseBytes('1.5MB')->shouldReturn(1572864);
     }
 
     public function it_parses_a_gigabyte_amount(): void
     {
-        $this->parseBytes('1.00GB')->shouldReturn(1024 * 1024 * 1024);
+        $this->parseBytes('1.00GB')->shouldReturn(1073741824);
+        $this->parseBytes('1.5GB')->shouldReturn(1610612736);
     }
 
     public function it_parses_a_terabyte_amount(): void
     {
-        $this->parseBytes('1.00TB')->shouldReturn(1024 * 1024 * 1024 * 1024);
+        $this->parseBytes('1.00TB')->shouldReturn(1099511627776);
+        $this->parseBytes('1.5TB')->shouldReturn(1649267441664);
     }
 }
